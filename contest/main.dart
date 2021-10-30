b7a7a6f9263ac2fe6e2a1971e95ad73acafdbfab
seyun24 <87846913+seyun24@users.noreply.github.com>
2021-10-30 19:36:14 +0900

    Create main.dart

---
+++ contest/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
+
+class WeatherScreen extends StatefulWidget {
+  WeatherScreen({this.parseWeatherData});
+  final dynamic parseWeatherData;
+  @override
+  _WeatherScreenState createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  String cityName;
+  int temp;
+  var date = DateTime.now();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updaterData(widget.parseWeatherData);
+  }
+
+  void updaterData(dynamic weatherData) {
+    double temp2 = weatherData['main']['temp'].toDouble();
+    cityName = weatherData['name'];
+    temp = temp2.round();
+
+    print(temp);
+    print(cityName);
+  }
+
+  String getSystemTime() {
+    var now = DateTime.now();
+    return DateFormat("h:mm a").format(now);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset(
+              'image/background.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 150.0,
+                                ),
+                                Text(
+                                  '$cityName',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 35.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    TimerBuilder.periodic(Duration(minutes: 1),
+                                        builder: (context) {
+                                      print('${getSystemTime()}');
+                                      return Text(
+                                        '${getSystemTime()}',
+                                        style: GoogleFonts.lato(
+                                          fontSize: 16.0,
+                                          color: Colors.white,
+                                        ),
+                                      );
+                                    }),
+                                    Text(DateFormat(' - EEEE,').format(date),
+                                        style: GoogleFonts.lato(
+                                          fontSize: 16.0,
+                                          color: Colors.white,
+                                        )),
+                                    Text(DateFormat('d MMM, yyy').format(date),
+                                        style: GoogleFonts.lato(
+                                          fontSize: 16.0,
+                                          color: Colors.white,
+                                        )),
+                                  ],
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  '$temp\u2103',
+                                  style: GoogleFonts.lato(
+                                    fontSize: 70.0,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ]),
+                    ),
+                    Column(
+                      children: [
+                        Divider(
+                          height: 15.0,
+                          thickness: 2.0,
+                          color: Colors.white30,
+                        ),
+                      ],
+                    )
+                  ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
